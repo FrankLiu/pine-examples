11cec63bb3bc2a1c55fa4a43ca859a06c87453a9
Frank Liu <frankliu@github.comm>
2018-09-01 22:40:42 +0800

    add CarrierMovementRepository unit test for cargo

--- cargo/src/test/java/io/pine/examples/cargo/infrastructure/persistence/CarrierMovementRepositoryTest.java
+++ cargo/src/test/java/io/pine/examples/cargo/infrastructure/persistence/CarrierMovementRepositoryTest.java
@@ -1,7 +1,9 @@
 package io.pine.examples.cargo.infrastructure.persistence;
 
 import io.pine.examples.cargo.domain.model.location.Location;
+import io.pine.examples.cargo.domain.model.location.SampleLocations;
 import io.pine.examples.cargo.domain.model.location.UnLocode;
+import io.pine.examples.cargo.domain.model.voyage.SampleVoyages;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -30,12 +32,19 @@ public class CarrierMovementRepositoryTest {
 
     @Before
     public void setup() {
-        locationRepository.saveAll(TestDataFixture.buildLocationData());
+        locationRepository.saveAll(SampleLocations.getAll());
+        voyageRepository.saveAll(Arrays.asList(
+                SampleVoyages.HONGKONG_TO_NEW_YORK,
+                SampleVoyages.NEW_YORK_TO_DALLAS,
+                SampleVoyages.DALLAS_TO_HELSINKI,
+                SampleVoyages.HELSINKI_TO_HONGKONG,
+                SampleVoyages.DALLAS_TO_HELSINKI_ALT));
     }
 
     @After
     public void teardown() {
         locationRepository.deleteAll();
+        voyageRepository.deleteAll();
     }
 
     @Test
